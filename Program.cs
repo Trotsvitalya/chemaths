3d38eaaacab3c75cf1e9072ba5203710b52ae4e1
Виталий Троц <trotz@zp.edu.ua>
2022-12-25 19:25:45 +0200

    Added Matrix and Balancer classes and actions for button in CalculatorWindows form

--- Program.cs
+++ Program.cs
@@ -8,9 +8,6 @@ namespace chemaths
 {
     static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
         [STAThread]
         static void Main()
         {
