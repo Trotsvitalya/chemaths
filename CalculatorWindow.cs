128ceea43f5e779b8e606e45fd4375b27aaf1dec
Hrachova <104226071+Hrachova@users.noreply.github.com>
2023-01-14 12:11:56 +0200

    add comments for CalculatorWindow

--- CalculatorWindow.cs
+++ CalculatorWindow.cs
@@ -19,39 +19,47 @@ namespace chemaths
 
         private void close_btn_Click(object sender, EventArgs e)
         {
-            Application.Exit();
+            Application.Exit();//закриття програми
+        }
+
+        private void OPEN_MENU_POINT(Form point_form, object sender)//функція відкриття форми
+        {
+            point_form.Show();
+            //задання границь попереднього вікна
+            point_form.SetBounds(this.Location.X, this.Location.Y, this.Width, this.Height);
+            this.Hide();
         }
 
         private void back_btn_Click(object sender, EventArgs e)
         {
-            ActiveMainWindow tmp = new ActiveMainWindow();
-            tmp.Show();
-            tmp.SetBounds(this.Location.X, this.Location.Y, this.Width, this.Height);
-            this.Hide();
+            OPEN_MENU_POINT(new ActiveMainWindow(), sender);//перехід до попереднього вікна
         }
 
         private void input_box_Click(object sender, EventArgs e)
         {
-            if(input_box.Text == "Cu + O2 -> CuO")
+            //якщо користувач не вводив нічого в поле та вміст поля дорівнює допоміжній підказці
+            if (input_box.Text == "Cu + O2 -> CuO")
             {
-                input_box.Text = null;
-                input_box.Font = new Font(input_box.Font, FontStyle.Regular);
-                input_box.ForeColor = Color.FromArgb(31, 34, 53);
+                input_box.Text = null;//очищення поля
+                input_box.Font = new Font(input_box.Font, FontStyle.Regular);//зняття курсиву
+                input_box.ForeColor = Color.FromArgb(31, 34, 53);//зміна кольору
             }
         }
 
         private void input_box_Leave(object sender, EventArgs e)
         {
+            //якщо поле порожнє
             if(string.IsNullOrWhiteSpace(input_box.Text))
             {
-                input_box.Text = "Cu + O2 -> CuO";
-                input_box.Font = new Font(input_box.Font, FontStyle.Italic);
-                input_box.ForeColor = Color.DimGray;
+                input_box.Text = "Cu + O2 -> CuO";//виведення допоміжної підказки
+                input_box.Font = new Font(input_box.Font, FontStyle.Italic);//встановлення курсиву
+                input_box.ForeColor = Color.DimGray;//зміна кольору
             }
         }
 
         private void active_panel_MouseDown(object sender, MouseEventArgs e)
         {
+            //функція перетягування безрамкового вікна за допоміжну активну панель
             active_panel.Capture = false;
             Message m = Message.Create(base.Handle, 0xa1, new IntPtr(2), IntPtr.Zero);
             WndProc(ref m);
@@ -59,18 +67,21 @@ namespace chemaths
 
         private void help_btn_Click(object sender, EventArgs e)
         {
-            help_panel.Visible = true;
+            help_panel.Visible = true;//відображення допоміжних підказок
+            //вимкнення відповіді елементів на дії користувача
             input_box.Enabled = help_btn.Enabled = output_box.Enabled = back_btn.Enabled = calculate_btn.Enabled = false;
         }
 
         private void help_close_btn_Click(object sender, EventArgs e)
         {
-            help_panel.Visible = false;
+            help_panel.Visible = false;//закриття допоміжних підказок
+            //увімкнення відповіді елементів на дії користувача
             input_box.Enabled = help_btn.Enabled = output_box.Enabled = back_btn.Enabled = calculate_btn.Enabled = true;
         }
         
         private void calculate_btn_Click_1(object sender, EventArgs e)
         {
+            //розстановка коефіцієнтів рівняння та виведення результату
             Balancer.eq = input_box.Text;
             Balancer.runs();
             output_box.Text = Balancer.final;
