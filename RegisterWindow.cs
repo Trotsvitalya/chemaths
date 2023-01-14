8c4d0786d26a748c71acaa72b14535f0ab3aefcf
Hrachova <104226071+Hrachova@users.noreply.github.com>
2023-01-14 12:44:09 +0200

    add comments for RegisterWindow

--- RegisterWindow.cs
+++ RegisterWindow.cs
@@ -18,41 +18,51 @@ namespace chemaths
             yes_btn.Checked = true;
         }
 
+        private void OPEN_MENU_POINT(Form point_form, object sender)//функція відкриття форми
+        {
+            point_form.Show();
+            //задання границь попереднього вікна
+            point_form.SetBounds(this.Location.X, this.Location.Y, this.Width, this.Height);
+            this.Hide();
+        }
+
         private void nickname_box_Leave(object sender, EventArgs e)
         {
+            //якщо поле порожнє
             if (string.IsNullOrWhiteSpace(nickname_box.Text))
             {
-                nickname_box.Text = "Куриленко Дмитро";
+                nickname_box.Text = "Куриленко Дмитро";//виведення допоміжної підказки
+                //встановлення курсиву
                 nickname_box.Font = new Font(nickname_box.Font, FontStyle.Italic);
-                nickname_box.ForeColor = Color.DimGray;
+                nickname_box.ForeColor = Color.DimGray;//зміна кольору
             }
         }
 
         private void nickname_box_Click(object sender, EventArgs e)
         {
+            //якщо вміст поля дорівнює вмісту допоміжної підказки
             if (nickname_box.Text == "Куриленко Дмитро")
             {
-                nickname_box.Text = null;
+                nickname_box.Text = null;//очищення поля
+                //зняття курсиву
                 nickname_box.Font = new Font(nickname_box.Font, FontStyle.Regular);
-                nickname_box.ForeColor = Color.FromArgb(31, 34, 53);
+                nickname_box.ForeColor = Color.FromArgb(31, 34, 53);//зміна кольору
             }
         }
 
         private void close_btn_Click(object sender, EventArgs e)
         {
-            Application.Exit();
+            Application.Exit();//закриття програми
         }
 
         private void back_btn_Click(object sender, EventArgs e)
         {
-            GameWindow tmp = new GameWindow();
-            tmp.Show();
-            tmp.SetBounds(this.Location.X, this.Location.Y, this.Width, this.Height);
-            this.Hide();
+            OPEN_MENU_POINT(new GameWindow(), sender);//перехід до попереднього вікна
         }
 
         private void active_panel_MouseDown(object sender, MouseEventArgs e)
         {
+            //функція перетягування безрамкового вікна за допоміжну активну панель
             active_panel.Capture = false;
             Message m = Message.Create(base.Handle, 0xa1, new IntPtr(2), IntPtr.Zero);
             WndProc(ref m);
@@ -65,20 +75,21 @@ namespace chemaths
             {
                 result_choice = "no";
             }
+         
             LevelWindow tmp = new LevelWindow(this, result_choice);
-            tmp.userName = nickname_box.Text;
-            tmp.Show();
-            tmp.SetBounds(this.Location.X, this.Location.Y, this.Width, this.Height);
-            this.Hide();
+            OPEN_MENU_POINT(tmp, sender);//перехід до наступного вікна
+            tmp.userName = nickname_box.Text;//передача ім'я користувача
         }
 
         private void level_choice_ValueChanged(object sender, EventArgs e)
         {
+            //зміна інформаційного поля в реальному часі
             info_label.Text = nickname_box.Text + "\nпочинає практику на рівні " + level_choice.Value;
         }
 
         private void nickname_box_TextChanged(object sender, EventArgs e)
         {
+            //зміна інформаційного поля в реальному часі
             info_label.Text = nickname_box.Text + "\nпочинає практику на рівні " + level_choice.Value;
         }
     }
