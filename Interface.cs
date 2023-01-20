7555197944a6474ac160a5f3d62b85f28847a54a
Hrachova <104226071+Hrachova@users.noreply.github.com>
2023-01-20 20:24:00 +0200

    edit Interface class

--- Interface.cs
+++ Interface.cs
@@ -3,14 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace chemaths
 {
-    public abstract class Interface : Form
+    class Interface
     {
-        public abstract void OPEN_MENU_POINT(Form point_form, object sender);
-        public abstract void close_btn_Click(object sender, EventArgs e);
-        public abstract void active_panel_MouseDown(object sender, MouseEventArgs e);
     }
 }
