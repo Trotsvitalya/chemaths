37c7452f0c8cdcdb5ae1d9178e991992206ce8cb
lazarchuk410 <lazarhuk410@gmail.com>
2023-01-20 19:38:55 +0200

    add abstract class Interface

---
+++ Interface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chemaths
+{
+    public abstract class Interface : Form
+    {
+        public abstract void OPEN_MENU_POINT(Form point_form, object sender);
+        public abstract void close_btn_Click(object sender, EventArgs e);
+        public abstract void active_panel_MouseDown(object sender, MouseEventArgs e);
+    }
+}
