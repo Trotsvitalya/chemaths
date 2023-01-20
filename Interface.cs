aac67aa8f11e03d716b3e049259415ba57a45a9c
lazarchuk410 <lazarhuk410@gmail.com>
2023-01-20 20:57:28 +0200

    add class Interface

--- Interface.cs
+++ Interface.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace chemaths
 {
-    class Interface
+    public class Interface : Form
     {
+        public void OPEN_MENU_POINT(Form point_form, object sender)//функція відкриття форми
+        {
+            point_form.Show();
+            //задання границь попереднього вікна
+            point_form.SetBounds(this.Location.X, this.Location.Y, this.Width, this.Height);
+            this.Hide();
+        }
     }
 }
