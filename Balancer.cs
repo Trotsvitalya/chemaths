caab640270dccf718b21cc359dd25707e639112a
Hrachova <104226071+Hrachova@users.noreply.github.com>
2023-01-20 23:06:17 +0200

    edit Exceptions.cs

--- Balancer.cs
+++ Balancer.cs
@@ -16,7 +16,7 @@ namespace chemaths
 
         public static void runs()
         {
-            ExceptionHandlingBalancer.PARSE(eq); //обробка виключних ситуацій
+            Exceptions.PARSE(eq); //обробка виключних ситуацій
 
             //отримуємо список усіх елементів та хімічних речовин
             ArrayList elements = AllUniqueElements(eq); //Приклад: H
@@ -556,116 +556,6 @@ namespace chemaths
             A.ConvertToSquare(1);
 
             return A;
-        }
-    }
-
-    //клас з обробкою виключних ситуацій
-    class ExceptionHandlingBalancer : Balancer
-    {
-        public static void PARSE(string eq)
-        {
-            //обробка ситуації, коли рівняння містить символи не тільки латинської абетки
-            if (Regex.IsMatch(eq, @"\p{IsCyrillic}"))
-            {
-                throw new Exception("alphabet_mistake");
-            }
-
-            //обробка ситуації, коли молекула містить пробіли
-            Regex regex = new Regex(@"[a-zA-Z]\s[A-Z]");
-            if (regex.IsMatch(eq))
-            {
-                throw new Exception("space_mistake");
-            }
-
-            //обробка ситуації, коли рівняння не містить хоча б одного знаку " + "
-            if (!eq.Contains(" + "))
-            {
-                throw new Exception("plus_mistake");
-            }
-            
-            //обробка ситуації, коли рівняння містить більше або менше 2 сторін, розділених " -> "
-            string[] sides = eq.Split(new string[] { " -> " }, StringSplitOptions.None);
-            if (sides.Length != 2)
-            {
-                throw new Exception("sides_mistake");
-            }
-
-            //обробка ситуації, коли рівняння не містить пробілів або містить більше, ніж 1 пробіл
-            //підряд
-            regex = new Regex(@"\s{2,}");
-            if (regex.IsMatch(eq) || !eq.Contains(' '))
-            {
-                throw new Exception("space_mistake");
-            }
-
-            PARSE_SIDE(sides[0]);
-            PARSE_SIDE(sides[1]);
-        }
-
-        public static void PARSE_SIDE(string side)
-        {
-            //розбиття сторони рівняння на молекули
-            string[] separators = { "+" };
-            string[] molecules = side.Split(separators, StringSplitOptions.RemoveEmptyEntries);
-            foreach (string molecule in molecules)
-            {
-                PARSE_MOLECULE(molecule);
-            }
-        }
-
-        public static void PARSE_MOLECULE(string molecule)
-        {
-            molecule = molecule.Trim();//прибирання пробілів на початку та на кінці рядка
-            //періодична таблиця хімічних елементів
-            string periodical_table = "Ac|Ag|Al|Am|Ar|As|At|Au|B|Ba|Be|Bh|Bi|Bk|Br|C|Ca|Cd|Ce|Cf|" +
-                                        "Cl|Cm|Co|Cr|Cs|Cu|Ds|Db|Dy|Er|Es|Eu|F|Fe|Fm|Fr|Ga|Gd|Ge|H|" +
-                                        "He|Hf|Hg|Ho|Hs|I|In|Ir|K|Kr|La|Li|Lr|Lu|Md|Mg|Mn|Mo|Mt|N|" +
-                                        "Na|Nb|Nd|Ne|Ni|No|Np|O|Os|P|Pa|Pb|Pd|Pm|Po|Pr|Pt|Pu|Ra|Rb|" +
-                                        "Re|Rf|Rg|Rh|Rn|Ru|S|Sb|Sc|Se|Sg|Si|Sm|Sn|Sr|Ta|Tb|Tc|Te|Th|" +
-                                        "Ti|Tl|Tm|U|V|W|Xe|Y|Yb|Zn|Zr";
-            string[] atoms = periodical_table.Split('|');
-            int counter = 0;
-            for (int i = 0; i < molecule.Length; i++)
-            {
-                //зчитання першого символу
-                char c = molecule[i];
-                string atom = c.ToString();
-
-                if (Regex.IsMatch(atom, "[A-Z]"))//якщо символ належить до верхнього регістру
-                {
-                    //перевірка символів після першого верхнього регістру
-                    for (int j = i + 1; j < molecule.Length; j++)
-                    {
-                        //зчитання наступного сиволу
-                        char d = molecule[j];
-                        string b = d.ToString();
-
-                        if (Regex.IsMatch(b, "[a-z]"))//якщо символ належить до нижнього регістру
-                        {
-                            atom += b;//створення атома, наприклад "С + u = Cu"
-                        }
-                        if (Regex.IsMatch(b, "[A-Z]"))//якщо символ належить до верхнього регістру
-                        {
-                            i = j - 1;//початок обробки з першого циклу
-                            break;
-                        }
-                    }
-                    counter = 0;
-                }
-
-
-                foreach (string element in atoms)
-                {
-                    if (atom == element)//якщо утворений атом існує в періодичній таблиці
-                    {
-                        counter = 1;
-                    }
-                }
-                if (counter != 1)//якщо утворений атом не існує в періодичній таблиці
-                {
-                    throw new Exception("atom_mistake");
-                }
-            }
         }
     }
 }
--- Interface.cs
+++ Interface.cs
@@ -10,53 +10,6 @@ namespace chemaths
 {
     class Interface
     {
-        public static bool active_tool_tips { get; set; }// перемінна для включення/виключення підказок
-
-        public static void OPEN_MENU_POINT(Form point_form, Form this_form)//функція відкриття форми
-        {
-            point_form.Show();
-            //задання границь попереднього вікна
-            point_form.SetBounds(this_form.Location.X, this_form.Location.Y, this_form.Width, this_form.Height);
-            this_form.Hide();
-        }
-
-        public static void ACTIVE_BTN(object sender, Button current_btn, Panel side_panel)//функція виділення обраного пункту меню
-        {
-            if (sender != null && current_btn != (Button)sender)
-            {
-                current_btn = (Button)sender;
-                current_btn.BackColor = Color.FromArgb(31, 34, 53);
-                current_btn.ForeColor = Color.White;
-                side_panel.Height = current_btn.Height;
-                side_panel.Top = current_btn.Top;
-            }
-        }
-
-        public static void INACTIVE_BTN(Panel menu_panel)//функція зняття виділення пунктів меню
-        {
-            //для кожної кнопки на панелі menu_panel зміна кольору тексту та заднього фону
-            foreach (Control previous_btn in menu_panel.Controls)
-            {
-                if (previous_btn.GetType() == typeof(Button))
-                {
-                    previous_btn.BackColor = Color.FromArgb(36, 41, 61);
-                    previous_btn.ForeColor = Color.FromArgb(11, 107, 210);
-                }
-            }
-        }
-
-        public static void HELP_CLOSE(Panel help_panel, TextBox input_box, TextBox output_box, PictureBox help_btn, PictureBox back_btn, PictureBox calculate_btn)
-        {
-            help_panel.Visible = false;//закриття допоміжних підказок
-            //увімкнення відповіді елементів на дії користувача
-            input_box.Enabled = help_btn.Enabled = output_box.Enabled = back_btn.Enabled = calculate_btn.Enabled = true;
-        }
-
-        public static void HELP_OPEN(Panel help_panel, TextBox input_box, TextBox output_box, PictureBox help_btn, PictureBox back_btn, PictureBox calculate_btn)
-        {
-            help_panel.Visible = true;//закриття допоміжних підказок
-            //увімкнення відповіді елементів на дії користувача
-            input_box.Enabled = help_btn.Enabled = output_box.Enabled = back_btn.Enabled = calculate_btn.Enabled = false;
-        }
+        
     }
 }
